8fe571c04f1dc8d659faa7c5be068539ab5d4f4e
ecodings <97072703+ecodings@users.noreply.github.com>
2025-06-21 13:11:27 +0900

    Create 00_image.py

---
+++ page/00_image.py
@@ -0,0 +1,2 @@
+import steamlit as st
+st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm5kMGIxejByMjJ3b3p5MTczbXdkZndpcGVodzE0NWxkYWgxbGpmdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LzwcNOrbA3aYvXK6r7/giphy.gif")
